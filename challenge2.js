8494887510680cdb2b4b6858abb37d09016693d0
Simon Conboy <85496061+Conboy-S@users.noreply.github.com>
2022-03-22 12:37:33 +0000

    commit 2
    all challenges complete

--- challenge2.js
+++ challenge2.js
@@ -20,10 +20,32 @@ function fibonacci(num) {
 fibonacci(1000);
 console.log(array1); // code for a fibonacci sequence to input(num) Attempt 1 failed*/
 
-const fibonacci = n =>
+/*const fibonacci = n =>
   Array.from({ length: n }).reduce(
     (acc, val, i) => acc.concat(i > 1 ? acc[i - 1] + acc[i - 2] : i),
     []
   );
 
-console.log(fibonacci());  //w3 schools solution 
+console.log(fibonacci()); w3 schools solution  */
+
+//Attempt 2
+
+function fibonacci() { // no input required
+    let a = 0; //first number in fib sequence
+    let b = 1; //second number in fib sequence
+    let c = 0; //total of even numbers in fob loop 
+    for(i = 0; ; i++) { //begins for loop
+        let d = a + b; //d is total of previous 2 numbers in sequence
+        if(d > 4000000){ //terminates loop once total exceeds 4 million
+            break;
+        }
+        if(d % 2 === 0) { //calculates if total(d) is even 
+            c += d; // if it is it is added to c, the total of even numbers
+        }
+        a = b; //increases first and second numbers for next iteration of loop
+        b = d;
+    }
+    console.log(c);
+}
+
+fibonacci();
--- challenge3.js
+++ challenge3.js
@@ -1,16 +1,21 @@
 /* Calculate prime factors of 600851475143 and store in array
 display largest of these */
-
+let array1 = []; // creates empty array
 function primeFind(integer) {
-    let array1 = []; // empty array
-    let prime; // 
+    let divisor =2; // starting divisor for loop
 
-    for (let i = 2; i <= integer; i++) { 
-    if (integer % i !== 0) continue;
-
-    for (let j = 2; j <= i / 2; j++) {
-      isPrime = i % j !== 0;
+    while (integer >=2) { //takes input and begins looping
+        if (integer % divisor === 0){ //calculates whether input has a remainder after divisor used 
+            integer = integer / divisor; // modifies input for further iterations
+            array1.push(divisor); // if so it is prime and added to the array
+        } else {
+            divisor++; //increments the divisor and repeats
+        }
     }
+    return array1; //produces array of prime numbers
 }
+
+array1=primeFind(600851475143);
+console.log(Math.max(...array1));
 
 
